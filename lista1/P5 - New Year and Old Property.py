067844b3135d081f3a2fb582fc7e461a249dbfaf
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-10-17 21:41:11 -0300

    Update P5 - New Year and Old Property.py

--- lista1/P5 - New Year and Old Property.py
+++ lista1/P5 - New Year and Old Property.py
@@ -1,36 +1,15 @@
-a, b = map(int, input().split(" "))
-ab = str(bin(a)[2:])
-bb = str(bin(b)[2:])
+a, b = map(int, input().split())
 
-count_ab = 0
-for e in ab:
-  if e == "0":
-    break
-  count_ab += 1
-
-count_bb = 0
-for e in bb:
-  if e == "0":
-    break
-  count_bb += 1
-
+candidate=0
 limark_count = 0
-if len(ab) != len(bb):    
-  for b in range(len(ab) + 1, len(bb)):
-    limark_count += b - 1
-  limark_count += len(ab) - count_ab
-  limark_count += count_bb - 1
+for i in range(1, 64):
+  ip = 2 ** i
+  for j in range(i - 1):
+    jp = 2 ** j
+    candidate = ip - jp - 1
+    if candidate >= a and candidate <= b:
+      limark_count += 1
 
-else:
-  tmp = 0
-  for i in range(ab):
-    if ab[i] != bb[i]:
-      tmp = i
-      break
-  
-  if tmp == 0:
-    limark_count = tmp
-  else:
-    print()
-    
+  if candidate >= b: break
+
 print(limark_count)
