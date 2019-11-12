2cb9645f624cc9a48da3340fc9ad94a73b5e56f3
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-12 14:26:51 -0300

    Update P1 - Equivalent Strings.cpp

--- lista2/P1 - Equivalent Strings.cpp
+++ lista2/P1 - Equivalent Strings.cpp
@@ -1,58 +1,47 @@
 # include <bits/stdc++.h>
 using namespace std;
-
-string a;
-string b;
+ 
 int slen;
+string a, b;
+ 
+bool compare_ranges(int a_init, int b_init, int size) {
+  for (int i = 0; i < size; i++) {
+    if (a[a_init+i] != b[b_init+i]) {
+      return false;
+    }    
+  }
+  return true;
+}
 
-void compare_odd_sized_string();
-void compare_pair_sized_string();
-bool compare(int a_init, int a_end, int b_init, int b_end);
+bool compare(int a_init, int b_init, int size){
+  if (compare_ranges(a_init, b_init, size)) {
+    return true;
+  }
+  if (size % 2 != 0){
+    return false;
+  }
+ 
+  int mid = size / 2;
+  int amid = a_init + mid;
+  int bmid = b_init + mid;
+  bool c2a1 = compare(a_init, b_init, mid);
+  if (c2a1 && compare(amid, bmid, mid)){
+    return true;
+  } 
+ 
+  bool c2b1 = compare(a_init, bmid, mid);
+  return c2b1 && compare(amid, b_init, mid);
+}
 
 int main() {  
   ios::sync_with_stdio(false);
   cin >> a;
   cin >> b;
   slen = a.length();
-  compare_odd_sized_string();
-  compare_pair_sized_string();
-}
-
-void compare_odd_sized_string() {
-  if (slen % 2 != 0){
-    if(a.compare(b) == 0){
-      cout << "YES";
-    } else {
-      cout << "NO";
-    }
+  if (compare(0, 0, slen)){
+    cout << "YES";
+  } else {
+    cout << "NO";
   }
-}
-
-void compare_pair_sized_string() {
-  if (slen % 2 == 0){
-    int end_index = slen - 1;
-    if (compare(0, end_index, 0, end_index)){
-      cout << "YES";
-    } else {
-      cout << "NO";
-    }
-  }
-}
-
-bool compare(int a_init, int a_end, int b_init, int b_end){
-  if (abs(a_end - a_init) == 0){
-    return a[a_init] == b[b_init];
-  }
-  int amid = (a_init + a_end) / 2;
-  int bmid = (b_init + b_end) / 2;
-  bool c2a1 = compare(a_init, amid, b_init, bmid);
-  bool c2a2 = compare(amid + 1, a_end, bmid + 1, b_end);
-  bool c2a = c2a1 and c2a2;
-  if (c2a){
-    return true;
-  } 
-  bool c2b1 = compare(a_init, amid, bmid + 1, b_end);
-  bool c2b2 = compare(amid + 1, a_end, b_init, bmid);
-  bool c2b = c2b1 and c2b2;
-  return c2b;
+  return 0;
 }
