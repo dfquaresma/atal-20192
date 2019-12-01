04539c31a7dcf3e89cf648ab22c873f6e1f98981
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-12-01 15:52:34 -0300

    Update P4 - Dark Roads.go

--- lista3/P4 - Dark Roads.go
+++ lista3/P4 - Dark Roads.go
@@ -20,29 +20,71 @@ type con struct {
   z int
 }
 
+var father []int
+var rank   []int
+var cons   []*con
 func main() {
   // STDOUT MUST BE FLUSHED MANUALLY!!!
   defer writer.Flush()
   var m, n int
-  scanf("%d %d\n", &m, &n)
-  cons := make([]*con, n)
-  var x, y, z, max int
-  for i := 0; i < n; i++ {
-    scanf("%d %d ", &x, &y)
-    if x == 0 && y == 0 {
-      break
+  for {
+    scanf("%d %d\n", &m, &n)
+    if m == 0 && n == 0 { break }
+    
+    cons  = make([]*con, n)
+    var x, y, z, max int
+    for i := 0; i < n; i++ {
+      scanf("%d %d ", &x, &y)
+      if x == 0 && y == 0 {
+        break
+      }
+      scanf("%d\n", &z)
+      cons[i]  = &con{x, y, z}
+      max += z
     }
-    scanf("%d\n", &z)
-    cons[i] = &con{x, y, z}
-    max += z
+    initFatherAndRank(m+1)
+    minimumCoverTreeSum := kruskal(m, n)
+    printf(strconv.Itoa(max - minimumCoverTreeSum))
   }
+}
+
+func kruskal(m, n int) int {
   sort.SliceStable(cons, func(i, j int) bool { return cons[i].z < cons[j].z })
-  kruskal := make([]int, m+1)
-  for i := 1; i <= m; i++ {
+  minimumCoverTreeSum := 0
+  for _, c := range cons {
+    if find(c.x) != find(c.y) {
+      union(c.x, c.y)
+      minimumCoverTreeSum += c.z
+    }
   }
-  var shortestPath int
-  for i := 1; i <= m; i++ {
-    shortestPath += kruskal[i]
-  }
-  printf(strconv.Itoa(max - shortestPath))
+  return minimumCoverTreeSum
+}
+
+func find(nodeIndex int) int {
+    if (father[nodeIndex] == nodeIndex) {
+      return nodeIndex
+    }
+    father[nodeIndex] = find(father[nodeIndex])
+    return father[nodeIndex]
+}
+
+func union(nodeAIndex, nodeBIndex int) {
+    nodeAIndex = find(nodeAIndex);
+    nodeBIndex = find(nodeBIndex);
+    if rank[nodeAIndex] < rank[nodeBIndex] {
+      tmp := nodeAIndex
+      nodeAIndex = nodeBIndex
+      nodeBIndex = tmp
+    }
+    rank[nodeAIndex]  += rank[nodeBIndex];
+    father[nodeBIndex] = father[nodeAIndex];
+}
+
+func initFatherAndRank(size int) {
+  father = make([]int, size)
+  rank   = make([]int, size)
+  for i := 1; i < size; i++ {
+    father[i] = i;
+    rank[i] = 1;
+  } 
 }
