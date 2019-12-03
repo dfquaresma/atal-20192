e13d0705f84397bf23e4424f2e9e3a867c9f157e
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-12-02 23:27:04 -0300

    Update P1 - Dijkstra?.py

--- lista3/P1 - Dijkstra?.py
+++ lista3/P1 - Dijkstra?.py
@@ -1,38 +1,21 @@
 # DESISTI DE FAZER EM GO...
+from heapq import *
 n, m = map(int, input().split())
-
+ 
 adjacencyMatrix  = {}
-def updateMatrix(a, b, w):
-  a_exist = adjacencyMatrix.get(a)
-  b_exist = adjacencyMatrix.get(b)
-
-  if a_exist and b_exist:
-    a_and_b_know_each_other = a_exist.get(b)
-    if a_and_b_know_each_other:
-      w = min(w, adjacencyMatrix[a][b])
-    adjacencyMatrix[a][b] = w
-    adjacencyMatrix[b][a] = w
-
-  else:
-    if a_exist:
-      adjacencyMatrix[a][b] = w
-      adjacencyMatrix[b] = {a: w}
-
-    elif b_exist:
-      adjacencyMatrix[b][a] = w
-      adjacencyMatrix[a] = {b: w}
-    
-    else:
-      adjacencyMatrix[a] = {b: w}
-      adjacencyMatrix[b] = {a: w}
-
-predecessor      = {}
 for i in range(m):
   a, b, w = map(int, input().split())
-  updateMatrix(a, b, w)
-  predecessor[a] = None
-  predecessor[b] = None
-
+  if not adjacencyMatrix.get(b): adjacencyMatrix[b] = {}
+  if not a in adjacencyMatrix[b] or adjacencyMatrix[b][a] > w:
+    adjacencyMatrix[b][a] = w
+  if not adjacencyMatrix.get(a): adjacencyMatrix[a] = {}
+  if not b in adjacencyMatrix[a] or adjacencyMatrix[a][b] > w:
+    adjacencyMatrix[a][b] = w
+ 
+predecessor = {}
+for i in range(1, n + 1):
+    predecessor[i] = None
+ 
 h = []
 distanceToRoot   = {}
 def relax(u, v):
@@ -40,43 +23,37 @@ def relax(u, v):
   if distanceToRootFromU == None:
     distanceToRootFromU = float('inf')
   distanceThroughU = distanceToRootFromU + adjacencyMatrix[u][v]
-
+ 
   distanceToRootFromV = distanceToRoot.get(v)
   if distanceToRootFromV == None:
     distanceToRootFromV = float('inf')
-
+ 
   if distanceToRootFromV > distanceThroughU:
     distanceToRoot[v] = distanceThroughU
     predecessor[v] = u
     heappush(h, (distanceThroughU, v))
-
+ 
 used = {}
-from heapq import *
-def extractMin():
-  return heappop(h)
-
 predecessor[1] = 1
 distanceToRoot[1] = 0
 heappush(h, (0, 1))
-i = 0
-while i < n:
-  u = extractMin()
+while len(h) > 0:
+  u = heappop(h)
   uid = u[1]
   if used.get(uid):
     continue
-  for nid, w in adjacencyMatrix[uid].items():
+  for nid, _ in adjacencyMatrix[uid].items():
     if not used.get(nid):
       relax(uid, nid)
   used[u] = True
-  i += 1
-
+ 
 output = str(n)
 nod = predecessor[n]
 while nod != 1 and nod != None:
   output = str(nod) + " " + output
   nod = predecessor[nod]
 output = "1 " + output
-
+ 
 if nod != None:
   print(output)
 else:
