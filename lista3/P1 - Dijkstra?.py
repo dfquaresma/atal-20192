0fb020161c1ac71c2c8af321814c7b4977735929
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-12-02 23:42:26 -0300

    Update P1 - Dijkstra?.py

--- lista3/P1 - Dijkstra?.py
+++ lista3/P1 - Dijkstra?.py
@@ -1,59 +1,52 @@
 # DESISTI DE FAZER EM GO...
 from heapq import *
 n, m = map(int, input().split())
- 
+
 adjacencyMatrix  = {}
 for i in range(m):
   a, b, w = map(int, input().split())
+  if a == b: continue
   if not adjacencyMatrix.get(b): adjacencyMatrix[b] = {}
   if not a in adjacencyMatrix[b] or adjacencyMatrix[b][a] > w:
     adjacencyMatrix[b][a] = w
   if not adjacencyMatrix.get(a): adjacencyMatrix[a] = {}
   if not b in adjacencyMatrix[a] or adjacencyMatrix[a][b] > w:
     adjacencyMatrix[a][b] = w
- 
+
+saw = {}
 predecessor = {}
+distanceToRoot = {}
 for i in range(1, n + 1):
     predecessor[i] = None
- 
+    distanceToRoot[i] = float('inf')
+    saw[i] = False
+
 h = []
-distanceToRoot   = {}
 def relax(u, v):
-  distanceToRootFromU = distanceToRoot.get(u)
-  if distanceToRootFromU == None:
-    distanceToRootFromU = float('inf')
-  distanceThroughU = distanceToRootFromU + adjacencyMatrix[u][v]
- 
-  distanceToRootFromV = distanceToRoot.get(v)
-  if distanceToRootFromV == None:
-    distanceToRootFromV = float('inf')
- 
-  if distanceToRootFromV > distanceThroughU:
+  distanceThroughU = distanceToRoot[u] + adjacencyMatrix[u][v]
+  if distanceToRoot[v] > distanceThroughU:
     distanceToRoot[v] = distanceThroughU
     predecessor[v] = u
     heappush(h, (distanceThroughU, v))
- 
-used = {}
+
 predecessor[1] = 1
 distanceToRoot[1] = 0
 heappush(h, (0, 1))
 while len(h) > 0:
   u = heappop(h)
   uid = u[1]
-  if used.get(uid):
-    continue
+  saw[uid] = True
   for nid, _ in adjacencyMatrix[uid].items():
-    if not used.get(nid):
+    if not saw[nid]:
       relax(uid, nid)
-  used[u] = True
- 
+
 output = str(n)
 nod = predecessor[n]
 while nod != 1 and nod != None:
   output = str(nod) + " " + output
   nod = predecessor[nod]
 output = "1 " + output
- 
+
 if nod != None:
   print(output)
 else:
